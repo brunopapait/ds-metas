4baebe6c4df69a8a72cdb5aa20cbbe4e2e6dc6e1
brunopapait <brunohpapait@gmail.com>
2022-07-18 20:45:56 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,20 @@
-import notificationIcon from '../../assets/img/notification-icon.svg';
-import './styles.css';
+import axios from "axios";
+import notificationIcon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
+import "./styles.css";
 
-export function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+export function NotificationButton({ saleId }: Props) {
+
+  function handleClick(saleId: number) {
+    axios.get(`${BASE_URL}/sales/${saleId}/notification`);
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={notificationIcon} alt="Notificar" />
     </div>
   );
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -62,7 +62,7 @@ export function SalesCard() {
           </thead>
           <tbody>
             {sales?.map((sale) => (
-              <tr>
+              <tr key={sale.id}>
                 <td className="show992">{sale.id}</td>
                 <td className="show576">
                   {new Date(sale.date).toLocaleDateString()}
@@ -73,7 +73,7 @@ export function SalesCard() {
                 <td>{sale.amount.toFixed(2)}</td>
                 <td>
                   <div className="dsmeta-red-btn-container">
-                    <NotificationButton />
+                    <NotificationButton saleId={sale.id} />
                   </div>
                 </td>
               </tr>
